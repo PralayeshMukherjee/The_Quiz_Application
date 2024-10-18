4b8028ce29d96302633e003a99cd249410d0e7e9
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-18 11:21:18 +0530

    test the files

---
+++ QuizeBackend/Quiz/src/main/java/com/raj/Quiz/entity/student.java
@@ -0,0 +1,10 @@
+package com.raj.Quiz.entity;
+
+public class student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String id;
+    private String password;
+}
